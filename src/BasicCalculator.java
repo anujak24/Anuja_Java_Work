30e09b4072502c46240c31e294d3ee59e8d267a0
anujak <anuja.kannawar24@gmail.com>
2025-03-20 18:52:53 +0530

    Calculator simpple

--- src/BasicCalculator.java
+++ src/BasicCalculator.java
@@ -1,9 +1,49 @@
 
 public class BasicCalculator {
+	int a = 10;
+	int b = 20;
+	int sum;
+	int diff;
+	int multi;
+	int div;
+	int mod;
+
+	public void add() {
+		sum = a + b;
+		System.out.println("Sum is " + sum + "\n");
+	}
+
+	public void diff() {
+		diff = a - b;
+		System.out.println("Differnce  is " + diff + "\n");
+
+	}
+
+	public void multi() {
+		multi = a * b;
+		System.out.println("Multiplication  is " + multi + "\n");
+
+	}
+
+	public void div() {
+		div = b / a;
+		System.out.println("Division  is " + div + "\n");
+
+	}
+
+	public void findMod() {
+		mod = b % a;
+		System.out.println("Mod  is " + mod + "\n");
+
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hello");
+		BasicCalculator basicCalculator = new BasicCalculator();
+		basicCalculator.add();
+		basicCalculator.diff();
+		basicCalculator.multi();
+		basicCalculator.div();
+		basicCalculator.findMod();
 
 	}
 
